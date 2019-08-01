7d697807bb3b7d8f93a02935cbf1438a38861860
Marcin Imielski <mimielski@syncad.com>
2019-08-01 14:26:39 +0200

    not working yet

--- app/shared/components/Wallet/Panel/Form/Jurisdictions.js
+++ app/shared/components/Wallet/Panel/Form/Jurisdictions.js
@@ -1,69 +1,69 @@
 // @flow
 import React, { Component } from 'react';
 import { I18n } from 'react-i18next';
-import { Form } from 'semantic-ui-react';
+import { Modal, Label, Form, Button, Header, Segment, Grid, Divider, Icon, Image } from 'semantic-ui-react';
 
 export default class JurisdictionsForm extends Component<Props> {
-  state = {
-    options: []
-  }
-
   componentDidMount() {
     const { actions, jurisdictions } = this.props;
     actions.getJurisdictions();
-    this.makeOptions(jurisdictions);
   }
 
-  makeOptions(jurisdictions) {
-    const options = [];
-    const j = jurisdictions.jurisdictions;
-    if (j) {
-      for (let i = 0; i < j.length; i += 1) {
-        const name = `${j[i].name} (${j[i].description})`;
-        options.push({ code: j[i].code, value: name, text: name });
-      }
-    }
-    this.setState({
-      options
-    });
-  }
-
-  changeValue() {
-    const newValue = [];
-    const oldValue = this.props.jurisdictions.choosenJurisdictions;
-    for (let i = 0; i < oldValue.length; i += 1) {
-      newValue.push(oldValue[i].value);
-    }
-    return newValue;
-  }
-
-  onChange(data) {
-    const jurisdictions = [];
-    for (let i = 0; i < data.value.length; i += 1) {
-      jurisdictions.push(data.options.find(o => o.value === data.value[i]));
-    }
-    this.props.actions.saveChoosenJurisdictions(jurisdictions);
+  handleEditClick(e) {
+    e.preventDefault();
   }
 
   render() {
-    const newValue = this.changeValue();
     const { label } = this.props;
 
     return (
       <I18n ns="wallet">
         {
           (t) => (
-            <Form.Dropdown
-              options={this.state.options}
-              placeholder="Select Jurisdiction"
-              fluid
-              search
-              multiple
-              selection
-              value={newValue}
-              onChange={(e, value) => this.onChange(value)}
-              label={label}
-            />
+            <div>
+              <Form.Group inline>
+                <label>{label}</label>
+              </Form.Group>
+              <Label>Hejka</Label>
+              <Modal
+                // centered={false}
+                trigger={
+                  <Button
+                    floated="right"
+                    size="mini"
+                    onClick={(e) => this.handleEditClick(e)}
+                  >
+                    Edit
+                  </Button>}
+              >
+                <Modal.Header>Select jurisdictions</Modal.Header>
+                <Modal.Content scrolling>
+
+                  <Modal.Description>
+                    <Segment>
+                      <Grid columns={2} stackable textAlign='center'>
+                        <Divider fitted vertical />
+
+                        <Grid.Row verticalAlign='middle'>
+                          <Grid.Column>
+                            xD
+                          </Grid.Column>
+
+                          <Grid.Column>
+                            xDD
+                          </Grid.Column>
+                        </Grid.Row>
+                      </Grid>
+                    </Segment>
+                  </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button primary>
+                    Close <Icon name='chevron right' />
+                  </Button>
+                </Modal.Actions>
+              </Modal>
+            </div>
           )
         }
       </I18n>
