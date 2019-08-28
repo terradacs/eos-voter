edc8b63b6b66015a1731afcc270b52666f0c0d84
Paul Olearski <p.olearski@wp.pl>
2019-08-28 10:14:15 +0200

    data transaction history does not reload, when api call fails show error, #30

--- app/shared/components/Wallet/Status/Actions/Table/JurisdictionHistoryRow.js
+++ app/shared/components/Wallet/Status/Actions/Table/JurisdictionHistoryRow.js
@@ -4,6 +4,9 @@ import { Grid } from 'semantic-ui-react';
 
 export default class JurisdictionHistoryRow extends Component<Props> {
 
+  leftError: false;
+  rightError: false;
+
   render() {
     const {
       leftRows,
@@ -13,22 +16,37 @@ export default class JurisdictionHistoryRow extends Component<Props> {
       t
     } = this.props;
 
+    if (currentSequence) {
+      if (jurisdictions.ALL_FOR_BLOCK === 'FAILURE') {
+        this.rightError = true;
+      } else if (jurisdictions.ALL_FOR_BLOCK === 'SUCCESS') {
+        this.rightError = false;
+      }
+    }
+    if (currentSequence) {
+      if (jurisdictions.ALL_FOR_TRANSACTION === 'FAILURE') {
+        this.leftError = true;
+      } else if (jurisdictions.ALL_FOR_TRANSACTION === 'SUCCESS') {
+        this.leftError = false;
+      }
+    }
+
     return (
       <Grid columns={2} divided>
         <Grid.Row>
           <Grid.Column>
             <label className="jurisdiction-label">{t('actions_table_history_one')}</label>
             <div className="history-scroll">
-              {leftRows.length > 0 && (currentSequence ? jurisdictions.ALL_FOR_TRANSACTION === 'SUCCESS' : true) && leftRows.map((row, idx) => (
+              {leftRows.length > 0 && (currentSequence ? jurisdictions.ALL_FOR_TRANSACTION === 'SUCCESS' : !this.leftError) && leftRows.map((row, idx) => (
                 <p key={idx} className="history-wrapper">{`${row.name} (${row.description})`}</p>
               ))}
-              {leftRows.length === 0 && (currentSequence ? jurisdictions.ALL_FOR_TRANSACTION === 'SUCCESS' : true) &&
+              {leftRows.length === 0 && (currentSequence ? jurisdictions.ALL_FOR_TRANSACTION === 'SUCCESS' : !this.leftError) &&
                 <p>No jurisdictions.</p>
               }
               {(currentSequence ? jurisdictions.ALL_FOR_TRANSACTION === 'PENDING' : false) &&
                 <p>Loading...</p>
               }
-              {currentSequence ? jurisdictions.ALL_FOR_TRANSACTION === 'FAILURE' : false &&
+              {(currentSequence ? jurisdictions.ALL_FOR_TRANSACTION === 'FAILURE' : this.leftError) &&
                 <p>Error fetching data.</p>
               }
             </div>
@@ -36,16 +54,16 @@ export default class JurisdictionHistoryRow extends Component<Props> {
           <Grid.Column>
             <label className="jurisdiction-label">{t('actions_table_history_two')}</label>
             <div className="history-scroll">
-              {rightRows.length > 0 && (currentSequence ? jurisdictions.ALL_FOR_BLOCK === 'SUCCESS' : true) && rightRows.map((row, idx) => (
+              {rightRows.length > 0 && (currentSequence ? jurisdictions.ALL_FOR_BLOCK === 'SUCCESS' : !this.rightError) && rightRows.map((row, idx) => (
                 <p key={idx} className="history-wrapper">{`${row.name} (${row.description})`}</p>
               ))}
-              {rightRows.length === 0 && (currentSequence ? jurisdictions.ALL_FOR_BLOCK === 'SUCCESS' : true) &&
+              {rightRows.length === 0 && (currentSequence ? jurisdictions.ALL_FOR_BLOCK === 'SUCCESS' : !this.rightError) &&
                 <p>No jurisdictions.</p>
               }
               {(currentSequence ? jurisdictions.ALL_FOR_BLOCK === 'PENDING' : false) &&
                 <p>Loading...</p>
               }
-              {currentSequence ? jurisdictions.ALL_FOR_BLOCK === 'FAILURE' : false &&
+              {(currentSequence ? jurisdictions.ALL_FOR_BLOCK === 'FAILURE' : this.rightError) &&
                 <p>Error fetching data.</p>
               }
             </div>
